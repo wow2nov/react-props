72ddcebcae0bce5873d40b33c695814d1e43d70e
Supapond <supapondmaliwab@gmail.com>
2026-04-15 17:29:01 +0700

    feat(app.jsx):component button

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function MainContent() {
           to collaborate with you to tackle your most complex challenges and
           drive your success.
         </p>
-        <Button buttonText="Learn More" />
+        <Button buttonText="Learn More" buttonStyle="learn-button" />
       </div>
     </main>
   );
@@ -70,7 +70,7 @@ function ProductSection() {
             />
             <h3>Premium Bluetooth Headphones</h3>
             <p className="product-price">2,400 Baht</p>
-            <Button buttonText="Add to Cart" buttonStyle="learn-button"/>
+            <Button buttonText="Add to Cart" buttonStyle="add-to-cart-button" />
           </div>
           <div className="product-card">
             <img
@@ -79,7 +79,7 @@ function ProductSection() {
             />
             <h3>Classic Leather Watch</h3>
             <p className="product-price">4,000 Baht</p>
-            <Button buttonText="Add to Cart"  buttonStyle="add-to-cart-button"/>
+            <Button buttonText="Add to Cart" buttonStyle="add-to-cart-button" />
           </div>
           <div className="product-card">
             <img
@@ -88,7 +88,10 @@ function ProductSection() {
             />
             <h3>Organic Green Tea</h3>
             <p className="product-price">79.99 Baht</p>
-            <Button buttonText="Add to Cart" buttonStyle="add-to-cart-button-secondary"/>
+            <Button
+              buttonText="Add to Cart"
+              buttonStyle="add-to-cart-button-secondary"
+            />
           </div>
         </div>
       </div>
@@ -105,7 +108,7 @@ function Footer() {
 }
 
 function Button(props) {
-  return <button className="add-to-cart-button">{props.buttonText}{props.buttonStyle}</button>;
+  return <button className={props.buttonStyle}>{props.buttonText}</button>;
 }
 
 function App() {
